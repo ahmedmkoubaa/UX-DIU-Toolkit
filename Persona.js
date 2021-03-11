3dc0f3ff1718622e26202290523b9c7842441ad0
ahmedmkoubaa <73902326+ahmedmkoubaa@users.noreply.github.com>
2021-03-11 21:23:46 +0100

    Add files via upload

--- Persona.js
+++ Persona.js
@@ -1,10 +1,10 @@
 /*******************************************/
 /*             PERSONA.JS                  */
-/*     Datos para PERSONA TEMPLATE         */   
-/*          [DIU] UX Toolkit v1.0 2019    */                        
+/*     Datos para PERSONA TEMPLATE         */
+/*          [DIU] UX Toolkit v1.0 2019    */
 /*          ver 1.1 26/Feb/2020            */
 /*******************************************/
-    
+
 /****  README:       */
 /****  Modifica los datos para las Personas      */
 /****  v.1.1 Incluye nombre de tu grupo de prácticas (Grupo.ID), curso académico y enlace a github ***/
@@ -16,94 +16,93 @@
 
 
 angular.module("angular", [])
-	.controller("controller", ["$scope", function($scope) { 
+	.controller("controller", ["$scope", function($scope) {
         $scope.Grupo_ID ="DIU1.ABCDEF";
         $scope.Curso ="2020/21";
         $scope.Github_ID ="https://github.com/mgea/UX-DIU-Toolkit";
-        
+
 		$scope.PersonaIndex = 0;
 		$scope.Personas = [
-			{		
-                
-                
-                /*************************************/
+			{
+
+					 /*************************************/
                 /**** PRIMERA PERSONA          *******/
                 /*** Cambiar datos             *******/
                 /*************************************/
-                
-                
+
+
 				Id: 0,
-				Name: "Pedro",
-				Photo: "man.png",
-				Quote: "Carpe Diem",
-				Age: 32,
-				Occupation: "Comercial en empresa de automocion",
-				Family: "Con pareja desde hace 1 año",
-				Location: "Granada (Huetor Vega)",
-				Character: "Le gusta disfrutar del tiempo",
+				Name: "Carlos",
+				Photo: "carlos.jpg",
+				Quote: "Disfrutar de la vida",
+				Age: 48,
+				Occupation: "Cajero",
+				Family: "Casado desde hace 18 años",
+				Location: "Málaga",
+				Character: "Le gusta hacer deporte",
 				PersonalityTraits: [
-					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 4 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 2 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 4 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 5 },
 					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 5 }
-				], 
-				Goals: ["Disfrutar del tiempo libre, viajar", "Cambiar a un trabajo más estable"],
-				Frustrations: ["le gusta la tecnología, pero siempre 'llama a un amigo' para resolver problemas", "Le gustaría tener más tiempo libre y leer más"],
-				Bio: "Es de Modril y vino a Granada para estudiar administración de empresas, pero no ha tenido grandes oportunidades de trabajo. LLeva 2 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y normalmente ser reunen para fiestas y a veces organizan viajes",
+				],
+				Goals: ["Conocer gente nueva y viajar", "Tener hijos"],
+				Frustrations: ["Su mujer no logra tener hijos", "No tiene amigos para jugar al futbol"],
+				Bio: "De joven le salió trabajo en Malaga y se quedo allí. Le encanta salir a tomar cervezas con los amigos",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 2 },
-					{ Name: "Movil", Value: 2 },
-					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 2 }
-					
-				], 
-                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",  
+					{ Name: "TIC/Internet", Value: 4 },
+					{ Name: "Movil", Value: 5 },
+					{ Name: "RRSS", Value: 2 },
+					{ Name: "Software", Value: 1 }
+
+				],
+                Contextos: "Está un poco cansado de su trabajo y le gustaria buscar alguno mejor pagado",
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional", Value: 5 },
-					{ Name: "Online & Social Media", Value: 3 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
+					{ Name: "Publicidad Tradicional", Value: 2 },
+					{ Name: "Online & Social Media", Value: 2 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 5 }
 				]
 			},
-			{	
-                
+			{
+
                 /*************************************/
                 /**** SEGUNDA PERSONA          *******/
                 /*** Cambiar datos             *******/
                 /*************************************/
-                
-                
+
+
 				Id: 1,
-				Name: "Monica Suarez",
-				Photo: "woman.png",
-				Quote: "A quotation that captures the essence of this person's personality",
-				Age: 17,
-				Occupation: "Searching for a cure for the Empress",
-				Family: "No parents, only family are the people who raised him.",
-				Location: "The Grassy Plains of Fantasia",
-				Character: "Strong, reliable and fearless.",
+				Name: "Heather Soto",
+				Photo: "heather.png",
+				Quote: "Mujer prevenida vale por tres",
+				Age: 32,
+				Occupation: "Abogada, madre de 3 niños, divorciada.",
+				Family: "Tiene 3 hijos",
+				Location: "Londres",
+				Character: "Fuerte, optimista y sin miedo.",
 				PersonalityTraits: [
-					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
-				], 
-				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
-				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 5 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 1 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 3 }
+				],
+				Goals: ["Ser feliz.", "Ver crecer s sus hijos.", "Encontrar el amor algun día."],
+				Frustrations: ["No ser engañada de nuevo", "Salir de casa sin paraguas.", "Que se le enfríen las lentejas."],
+				Bio: "Madre de 3 preciosos niños frutos de una relación fallida con su exmarido de quien está divorciada. Gran apasionada de su trabajo, ama la justicia y se prometió a sí misma jamás dejar a una mujer indefensa.",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 5 },
-					{ Name: "Mobile", Value: 3 },
-					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 5 }
-					
-				], 
-                Contextos:   "The goals this user hopes to achieve." ,
+					{ Name: "TIC/Internet", Value: 2 },
+					{ Name: "Mobile", Value: 2 },
+					{ Name: "RRSS", Value: 4 },
+					{ Name: "Software", Value: 1 }
+
+				],
+                Contextos:   "Llegar a ser feliz con sus niños." ,
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
-					{ Name: "Online & Social Media", Value: 2 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 2 }
+					{ Name: "Online & Social Media", Value: 4 },
+					{ Name: "Recomendaciones & sugerencias", Value: 5 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1  }
 				]
 			}
 		];
